4efe1cf729d7d70ba886854c6e0b6465f5379ad0
sanjay <bahasanjay27@gmail.com>
2024-04-12 12:31:17 +0545

    Week 1 Task 1 Question

--- Week 1/Hello.java
+++ Week 1/Hello.java
@@ -1,7 +1,8 @@
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello World");;
+    /* 1. Write a program that prints Hello World!! to the console. Save the program as Hello.java. */
+        System.out.println("Hello World");
     }
 
 }
