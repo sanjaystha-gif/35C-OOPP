a34ac03e3acd19f1451b3382fda758f446374d42
sanjay <bahasanjay27@gmail.com>
2024-04-26 11:38:08 +0545

    Week1 task completed

--- Week 1/Hello.java
+++ Week 1/Hello.java
@@ -48,7 +48,7 @@ public class Hello {
 
     ********* */
         System.out.print("*********\n*********\n*********\n*********\n");
-    }
+    
     
     /* 6. WAP that prints the following output:
 
@@ -63,6 +63,17 @@ public class Hello {
     a ' and a "?  Or between a " and a \"?
 
     One is what we see when we're typing our program. The other is what appears on the "console."
+
  */
+
+        System.out.println("A well-formed Java program has a main method with { and } braces.");
+        System.out.println("A System.out.println statement has ( and ) and usually a String that starts and ends with a \" character.");
+        System.out.println("(But we type \\\" instead!)");
+        System.out.println();
+        System.out.println("What is the difference between");
+        System.out.println("a ' and a \"?  Or between a \" and a \\\"?");
+        System.out.println();
+        System.out.println("One is what wee see when we're typing our program. The other is what appears on the \"console.\"");
+    }
 
 }
